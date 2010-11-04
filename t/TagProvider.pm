e0bc1ac2e78c42d378b2699ed99ea1c6ae67784f
Ricardo SIGNES <rjbs@cpan.org>
2010-11-04 04:32:17 +0000

    Import of RJBS/MooseX-ComposedBehavior-0.001 from CPAN.
    gitpan-cpan-distribution: MooseX-ComposedBehavior
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         RJBS/MooseX-ComposedBehavior-0.001.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ t/TagProvider.pm
@@ -0,0 +1,15 @@
+package t::TagProvider;
+use strict;
+
+use MooseX::ComposedBehavior -compose => {
+  sugar_name   => 'add_tags',
+  also_compose => '_instance_tags',
+  context      => 'list',
+  compositor   => sub {
+    my ($self, $results) = @_;
+    return map { @$_ } @$results if wantarray;
+  },
+  method_name  => 'tags',
+};
+
+1;
